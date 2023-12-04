c29f23962e2643e4b868fbb74259c4c6a98a947a
esther808 <ho.es@northeastern.edu>
2023-12-04 00:36:59 -0500

    updated files

--- app.js
+++ app.js
@@ -1,22 +1,43 @@
-import express from 'express';
-import Hello from './hello.js';
+// const express = require("express");
+import express from "express";
+import HelloRoutes from "./hello.js";
 import Lab5 from "./lab5.js";
-import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
-import ModuleRoutes from "./Modules/routes.js";
-import "dotenv/config";
-import session from "express-session";
+import ModuleRoutes from "./modules/routes.js";
+import cors from "cors";
 
-app.listen(process.env.PORT || 4001);
-// app.use(cors());
-// app.use(express.json());
-// const app = express()
-// app.use(express.json());
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+Lab5(app);
 ModuleRoutes(app);
 CourseRoutes(app);
-Lab5(app);
-Hello(app)
-const app = express();
-app.use(cors());
+HelloRoutes(app);
+app.listen(4000);
 
-// app.listen(4001)
+
+
+// import express from 'express';
+// import Hello from './hello.js';
+// import Lab5 from "./lab5.js";
+// import cors from "cors";
+// import CourseRoutes from "./courses/routes.js";
+// import ModuleRoutes from "./modules/routes.js";
+// import "dotenv/config";
+// import session from "express-session";
+
+// app.listen(process.env.PORT || 4001);
+// // app.use(cors());
+// // app.use(express.json());
+// // const app = express()
+// // app.use(express.json());
+// ModuleRoutes(app);
+// CourseRoutes(app);
+// Lab5(app);
+// Hello(app)
+// const app = express();
+// app.use(cors());
+
+// // app.listen(4001)
